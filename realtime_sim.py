a728b1a15205887002a9d020d2026499bf8ae877
emilykuowen <ekuo@usc.edu>
2022-03-03 16:53:52 -0800

    merged code

--- realtime_sim.py
+++ realtime_sim.py
@@ -4,38 +4,42 @@ from scipy import signal
 import matplotlib.pyplot as plt
 import sofa
 from pydub import AudioSegment
-import pydub
-import io
-import scipy.io.wavfile
 from scipy.io.wavfile import write
 import csv
-import pandas as pd
 import wave
 import pyaudio
 import wave
-import sys
 import time
+import math
 
-class AudioFile:
-    chunk = 1024
-
+class AudioStream:
     def __init__(self, file):
         """ Initialize """
         self.wf = wave.open(file, 'rb')
         self.p = pyaudio.PyAudio()
+
+        # print(self.wf.getsampwidth())
+        # print(self.p.get_format_from_width(self.wf.getsampwidth()))
+        # print(self.wf.getnchannels())
+        # print(self.wf.getframerate())
+
         self.stream = self.p.open(
-            format = self.p.get_format_from_width(self.wf.getsampwidth()),
-            channels = self.wf.getnchannels(),
-            rate = self.wf.getframerate(),
+            format = self.p.get_format_from_width(self.wf.getsampwidth()), # 16-bit int
+            channels = self.wf.getnchannels(), # 1 channel
+            rate = self.wf.getframerate(), # 44100 Hz
             output = True
         )
 
-    def play(self):
-        """ Play entire file """
-        data = self.wf.readframes(self.chunk)
-        while data != b'':
-            self.stream.write(data)
-            data = self.wf.readframes(self.chunk)
+    # def play(self):
+    #     """ Play entire file """
+    #     data = self.wf.readframes(self.chunk)
+    #     while data != b'':
+    #         self.stream.write(data)
+    #         data = self.wf.readframes(self.chunk)
+
+    def queueChunk(self, chunk):
+        """ Write audio to stream """
+        self.stream.write(chunk)
 
     def close(self):
         """ Close stream """
@@ -55,9 +59,9 @@ class HRTFFile:
 
     def getIR(self, azimuth, elevation):
         """ Access IR measurements """
-        self.measurement = findMeasurement(azimuth, elevation)
-        hrtf1 = HRTF.Data.IR.get_values(indices={"M":self.measurement, "R":0, "E":self.emitter})
-        hrtf2 = HRTF.Data.IR.get_values(indices={"M":self.measurement, "R":1, "E":self.emitter})
+        self.measurement = self.findMeasurement(azimuth, elevation)
+        hrtf1 = self.HRTF.Data.IR.get_values(indices={"M":self.measurement, "R":0, "E":self.emitter})
+        hrtf2 = self.HRTF.Data.IR.get_values(indices={"M":self.measurement, "R":1, "E":self.emitter})
         return [hrtf1, hrtf2]
 
     def findMeasurement(self, azimuth, elevation):
@@ -73,8 +77,8 @@ class HRTFFile:
                 bestError = currError
         return bestIndex
 
+
 class Listener:
-    
     def __init__(self):
         """ Initialilze """
         self.xPos = 0
@@ -90,8 +94,9 @@ class Listener:
         keyboard.add_hotkey('a', self.update(0, 0, 0, 1, 0))
         keyboard.add_hotkey('s', self.update(0, 0, 0, 0, -1))
         keyboard.add_hotkey('d', self.update(0, 0, 0, -1, 0))
-        keyboard.add_hotkey('space', self.update(0, 0, 1, 0, 0))
-        keyboard.add_hotkey('shift', self.update(0, 0, -1, 0, 0))
+        # keyboard.add_hotkey('space', self.update(0, 0, 1, 0, 0))
+        # keyboard.add_hotkey('shift', self.update(0, 0, -1, 0, 0))
+        self.update(0, 0, 0, 0, 0) # reset the positions
 
     def getPos(self):
         """ Access position info """
@@ -107,44 +112,52 @@ class Listener:
 
 
 class Scene:
-
     def __init__(self, sourceFilename, HRTFFilename):
         """ Initialize """
         self.listener = Listener()
         self.HRTF = HRTFFile(HRTFFilename)
-        #TODO use sourceFilename to open a .txt file (or something else) and create an array of source objects to store in self.sources[] array 
-        self.sources = [1, 0, 0, "test.wav"]
-        self.stream = AudioStream()
-        self.exit = False
-        self.chunkSize = 1024
+        #TODO read a text file that lists the source objects
+        #TODO use sourceFilename to open a .txt file and create an array of source objects to store in self.sources[] array 
+        self.sources = [Source(1, 0, 0, sourceFilename)]
+        self.stream = AudioStream(sourceFilename)
+        # the larger the chunk size, the less noise / pauses
+        self.chunkSize = 8192
         self.timeIndex = 0
         self.fs = 44100
+        self.exit = False
 
     def begin(self):
-        self.exit = False
-        keyboard.add_hotkey('q', self.quit())
+        # TODO make adding hotkey not call self.quit()
+        # keyboard.add_hotkey('q', self.quit())
+        chunkTime = 1.0 / self.fs * self.chunkSize
+        print(chunkTime)
+
         while(~self.exit):
             start_time = time.time()
-            chunkTime = self.fs * self.chunkSize
+            # TODO use a thread to separate generating and playing chunk
             self.generateChunk(self.timeIndex, self.chunkSize)
-            while(time.time()-start_time < chunktime):
+            print("current time = ", time.time() - start_time)
+            while((time.time() - start_time) < chunkTime):
                 pass
-                
             self.timeIndex = self.timeIndex + self.chunkSize
 
     def quit(self):
         self.exit = True
+        # self.stream.close()
 
     def generateChunk(self, timeIndex, chunkSize):
         """" Generate and queue an audio chunk """
-        #TODO as this is, it will spatialize an entire source object audio file every time this is called. Next, this needs to be split to play a smaller chunk at a time
         for currSource in self.sources:
             [azimuth, elevation] = self.getAngles(currSource)
+            print("azimuth = ", azimuth)
+            print("elevation = ", elevation)
             [hrtf1, hrtf2] = self.HRTF.getIR(azimuth, elevation)
 
             soundFile = currSource.getSound()
-            soundChunk = soundFile[timeIndex:timeIndex+chunkSize]
+            # TODO make this work for both mono and stereo files
+            soundChunk = soundFile[timeIndex:timeIndex+chunkSize, 0]
 
+            # TODO test using fft instead to see if it's faster
             convolved1 = np.array(signal.convolve(soundChunk, hrtf1, mode='full'))
             convolved2 = np.array(signal.convolve(soundChunk, hrtf2, mode='full'))
 
@@ -157,19 +170,35 @@ class Scene:
             #TODO adjust gain for inverse squared distance relationship
 
             #TODO check if this is the right way to add stereo audio data to stream
-            self.stream.queueChunk([convolved1, convolved2])
+            convolved = np.array([convolved1, convolved2])
+            norm = np.linalg.norm(convolved)
+            convolved_normalized = convolved / norm
+            num_bit = 16
+            bit_depth = 2 ** (num_bit-1)
+            convolved_final = np.int16(convolved_normalized / np.max(np.abs(convolved_normalized)) * (bit_depth-1))
+            self.stream.queueChunk(convolved_final.tobytes())
 
-    
+    # will have to take in the listener object as an argument
     def getAngles(self, source):
         """ Calculate azimuth and elevation angle from listener to object """
         [sourceX, sourceY, sourceZ] = source.getPos()
-        # TODO calculate azimuth and elevation angles from a given source object and listener object
-        azimuth = 0
-        elevation = 0
-        return [azimuth, elivation]
+        [listenerX, listenerY, listenerZ] = self.listener.getPos()
+        print("listenerX = ", listenerX)
+        print("listenerY = ", listenerY)
+        print("listenerZ = ", listenerZ)
+        
+        numerator = sourceY - listenerY
+        denominator = sourceX - listenerX
+        azimuth = math.atan(numerator / denominator)
+
+        numerator = sourceZ - listenerZ
+        denominator = math.sqrt( ((sourceX - listenerX)**2) + ((sourceY - listenerY)**2) )
+        elevation = math.atan(numerator / denominator)
+
+        return [azimuth, elevation]
+
 
 class Source:
-
     def __init__(self, x, y, z, filename):
         """ Initialize """
         self.xPos = x
@@ -179,7 +208,6 @@ class Source:
         segment = AudioSegment.from_file(filename)
         channel_sounds = segment.split_to_mono()
         samples = [s.get_array_of_samples() for s in channel_sounds]
-
         self.audioArray = np.array(samples).T
     
     def getPos(self):
@@ -190,22 +218,11 @@ class Source:
         """ Access audio info """
         return self.audioArray
 
-class AudioStream:
 
-    def __init__(self):
-        """ Initialize """
-        self.p = pyaudio.PyAudio()
-        self.stream = self.p.open(format = self.p.get_format_from_width(4), channels = 2, rate = 44100, output = True)
-    
-    def queueChunk(self, chunk):
-        """ Write audio to stream """
-        self.stream.write(chunk)
+if __name__ == "__main__":
+    currentScene = Scene("sin_440.wav", "mit_kemar_normal_pinna.sofa")
+    currentScene.begin()
 
-
-""" MAIN """
-
-currentScene = Scene("sources.txt", "mit_kemar_normal_pinna.sofa")
-currentScene.begin()
 
 ## TODO Scene(), Source() Figure out format for Source object files.
     ## Each source object should have some kind of txt or csv file containing info on its audio file and position data
