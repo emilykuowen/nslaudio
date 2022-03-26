0cee568126597023df62c10353a76a6568d7ddee
emilykuowen <ekuo@usc.edu>
2022-03-26 11:44:01 -0700

    debugging update

--- realtime_sim.py
+++ realtime_sim.py
@@ -18,24 +18,26 @@ class AudioStream:
         self.wf = wave.open(file, 'rb')
         self.p = pyaudio.PyAudio()
 
-        # print(self.wf.getsampwidth())
-        # print(self.p.get_format_from_width(self.wf.getsampwidth()))
-        # print(self.wf.getnchannels())
-        # print(self.wf.getframerate())
+        # http://people.csail.mit.edu/hubert/pyaudio/docs/
+        print(self.wf.getsampwidth()) # 2 --> 16-bit int
+        print(self.p.get_format_from_width(self.wf.getsampwidth())) # 8 --> 16-bit int
+        print(self.wf.getnchannels()) # 1
+        print(self.wf.getframerate()) # 44100
 
         self.stream = self.p.open(
             format = self.p.get_format_from_width(self.wf.getsampwidth()), # 16-bit int
-            channels = self.wf.getnchannels(), # 1 channel
+            channels = 2, # 1 channel
             rate = self.wf.getframerate(), # 44100 Hz
             output = True
         )
 
-    # def play(self):
-    #     """ Play entire file """
-    #     data = self.wf.readframes(self.chunk)
-    #     while data != b'':
-    #         self.stream.write(data)
-    #         data = self.wf.readframes(self.chunk)
+    def play(self):
+        """ Play entire file """
+        data = self.wf.readframes(1024)
+
+        while data != b'':
+            self.stream.write(data)
+            data = self.wf.readframes(1024)
 
     def queueChunk(self, chunk):
         """ Write audio to stream """
@@ -130,44 +132,56 @@ class Scene:
     def begin(self):
         # TODO make adding hotkey not call self.quit()
         # keyboard.add_hotkey('q', self.quit())
-        chunkTime = 1.0 / self.fs * self.chunkSize
-        print("chunk time = ", chunkTime)
+        # chunkTime = 1.0 / self.fs * self.chunkSize
+        # print("chunk time = ", chunkTime)
 
-        while(~self.exit):
-            start_time = time.time()
-            # TODO use a thread to separate generating and playing chunk
-            # TODO test code with writing audio data to an array instead of outputting right away
-            # TODO check how much time needs to be reduced / optimized
-            convolved_final = self.generateChunk(self.timeIndex, self.chunkSize)
-            print("chunk size = ", 1.0 / self.fs * len(convolved_final))
-            print("processing time = ", time.time() - start_time)
-            self.stream.queueChunk(convolved_final)
-            # while((time.time() - start_time) < chunkTime):
-            #     pass
-            self.timeIndex = self.timeIndex + self.chunkSize
+        chunk_size = 4096
+        data = self.stream.wf.readframes(chunk_size) # returns byte string
+        data_np = np.frombuffer(data, dtype=np.uint16) # returns int array of chunk_size if mono and chunk_size * 2 if stereo
+        
+        # print(list(data))
+        # print("data len = ", len(list(data)))
+        # print(data_np)
+        # print("data np len = ", len(data_np))
+
+        while data != b'':
+            data_np = np.frombuffer(data, dtype=np.uint16)
+            convolved = self.generateChunk(data_np, chunk_size)
+            self.stream.stream.write(convolved)
+            data = self.stream.wf.readframes(chunk_size)
+
+        # while(~self.exit):
+        #     start_time = time.time()
+        #     # TODO use a thread to separate generating and playing chunk
+        #     # TODO test code with writing audio data to an array instead of outputting right away
+        #     # TODO check how much time needs to be reduced / optimized
+        #     convolved_final = self.generateChunk(self.timeIndex, self.chunkSize)
+        #     print("chunk size = ", 1.0 / self.fs * len(convolved_final))
+        #     print("processing time = ", time.time() - start_time)
+        #     self.stream.queueChunk(convolved_final)
+        #     # while((time.time() - start_time) < chunkTime):
+        #     #     pass
+        #     self.timeIndex = self.timeIndex + self.chunkSize
 
     def quit(self):
         self.exit = True
         # self.stream.close()
 
-    def generateChunk(self, timeIndex, chunkSize):
-        """" Generate and queue an audio chunk """
+    def generateChunk(self, data, chunkSize):
+        """" Generate an audio chunk """
         for currSource in self.sources:
-            [azimuth, elevation] = self.getAngles(currSource)
-            print("azimuth = ", azimuth)
-            print("elevation = ", elevation)
-            [hrtf1, hrtf2] = self.HRTF.getIR(azimuth, elevation)
-
-            soundFile = currSource.getSound()
-            # TODO make this work for both mono and stereo files
-            soundChunk = soundFile[timeIndex:timeIndex+chunkSize, 0]
-            print("sound chunk size = ", len(soundChunk))
+            # [azimuth, elevation] = self.getAngles(currSource)
+            # print("azimuth = ", azimuth)
+            # print("elevation = ", elevation)
+            [hrtf1, hrtf2] = self.HRTF.getIR(0, 0)
+            print("data len = ", len(data))
+            print("hrtf1 len = ", len(hrtf1))
 
             # TODO test using fft instead to see if it's faster
-            convolved1 = np.array(signal.fftconvolve(soundChunk, hrtf1, mode='full'))
-            convolved2 = np.array(signal.fftconvolve(soundChunk, hrtf2, mode='full'))
+            convolved1 = np.array(signal.fftconvolve(data, hrtf1, mode='full'))
+            convolved2 = np.array(signal.fftconvolve(data, hrtf2, mode='full'))
             print("convolved1 size = ", len(convolved1))
-            print("convolved2 size = ", len(convolved2))
+            print("convolved2 size = ", len(convolved2)) 
 
             # start_index = min(np.flatnonzero(convolved1)[0], np.flatnonzero(convolved2)[0])
             # end_index = max(np.flatnonzero(convolved1)[len(np.flatnonzero(convolved1))-1], np.flatnonzero(convolved2)[len(np.flatnonzero(convolved2))-1])
@@ -176,14 +190,52 @@ class Scene:
 
             #TODO adjust gain for inverse squared distance relationship
 
-            #TODO check if this is the right way to add stereo audio data to stream
-            convolved = np.array([convolved1, convolved2])
+            convolved = np.array([convolved1, convolved2]).T
+            print(convolved.shape)
             norm = np.linalg.norm(convolved)
             convolved_normalized = convolved / norm
+            print(convolved_normalized.shape)
             num_bit = 16
             bit_depth = 2 ** (num_bit-1)
             convolved_final = np.int16(convolved_normalized / np.max(np.abs(convolved_normalized)) * (bit_depth-1))
-            return convolved_final.tobytes()
+            interleaved = convolved_final.flatten()
+            out_data = interleaved.tobytes()
+            return out_data
+    
+    # def generateChunk(self, timeIndex, chunkSize):
+    #     """" Generate an audio chunk """
+    #     for currSource in self.sources:
+    #         [azimuth, elevation] = self.getAngles(currSource)
+    #         print("azimuth = ", azimuth)
+    #         print("elevation = ", elevation)
+    #         [hrtf1, hrtf2] = self.HRTF.getIR(azimuth, elevation)
+
+    #         soundFile = currSource.getSound()
+    #         # TODO make this work for both mono and stereo files
+    #         soundChunk = soundFile[timeIndex:timeIndex+chunkSize, 0]
+    #         print("sound chunk size = ", len(soundChunk))
+
+    #         # TODO test using fft instead to see if it's faster
+    #         convolved1 = np.array(signal.fftconvolve(soundChunk, hrtf1, mode='valid'))
+    #         convolved2 = np.array(signal.fftconvolve(soundChunk, hrtf2, mode='valid'))
+    #         print("convolved1 size = ", len(convolved1))
+    #         print("convolved2 size = ", len(convolved2))
+
+    #         # start_index = min(np.flatnonzero(convolved1)[0], np.flatnonzero(convolved2)[0])
+    #         # end_index = max(np.flatnonzero(convolved1)[len(np.flatnonzero(convolved1))-1], np.flatnonzero(convolved2)[len(np.flatnonzero(convolved2))-1])
+    #         # convolved1 = convolved1[start_index:end_index]
+    #         # convolved2 = convolved2[start_index:end_index]   
+
+    #         #TODO adjust gain for inverse squared distance relationship
+
+    #         #TODO check if this is the right way to add stereo audio data to stream
+    #         convolved = np.array([convolved1, convolved2])
+    #         norm = np.linalg.norm(convolved)
+    #         convolved_normalized = convolved / norm
+    #         num_bit = 16
+    #         bit_depth = 2 ** (num_bit-1)
+    #         convolved_final = np.int16(convolved_normalized / np.max(np.abs(convolved_normalized)) * (bit_depth-1))
+    #         return convolved_final.tobytes()
 
     # will have to take in the listener object as an argument
     def getAngles(self, source):
@@ -228,6 +280,7 @@ class Source:
 
 if __name__ == "__main__":
     currentScene = Scene("sin_440.wav", "mit_kemar_normal_pinna.sofa")
+    # currentScene.stream.play()
     currentScene.begin()
 
 
